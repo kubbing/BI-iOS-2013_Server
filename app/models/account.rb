2f5cccec29d6afd210ba4542aaa5d660cb2299cc
Jakub Hladik <kubbing@me.com>
2013-11-09 20:40:35 +0100

    validations

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,10 @@
 class Account < ActiveRecord::Base
+	
+	validates_presence_of :login
+	validates_uniqueness_of :login
+	
+	validates_presence_of :nick
+	validates_uniqueness_of :nick
 
 	has_many :feeds, dependent: :destroy
 
